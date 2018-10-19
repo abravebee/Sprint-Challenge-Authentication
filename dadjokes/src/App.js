37a2cf95d2f52f13e8b842f65f05f19e35c6aba3
Casey Baker <caseye.baker@gmail.com>
2018-10-19 10:35:28 -0700

    still having node module issues; coding blind; jokes component built

--- dadjokes/src/App.js
+++ dadjokes/src/App.js
@@ -3,23 +3,47 @@ import { NavLink, Route } from 'react-router-dom';
 
 import './App.css';
 
+const Home = props => {
+  return (
+    <div>
+      <h1>Welcome.</h1>
+      <h3>Use the links above to navigate.</h3>
+    </div>
+  );
+};
+
 class App extends Component {
+
+  signout = () => {
+    localStorage.removeItem('jwt');
+  };
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
         <h1>Dad Jokes Database</h1>
           <nav>
-            <NavLink>
+            <NavLink
+              exact to="/">
+              Home
+            </NavLink>
+            <NavLink
+              exact to="/jokes">
               Jokes
             </NavLink>
-            <NavLink>
+            <NavLink
+              exact to="/signup">
               Sign Up
             </NavLink>
-            <NavLink>
+            <NavLink
+              exact to="/signin">
               Sign In
             </NavLink>
-            <NavLink>
+            <NavLink
+              exact to="/home"
+              onClick={this.signout}>
               Sign Out
             </NavLink>
           </nav>
---
+++ dadjokes/src/components/Jokes.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Jokes extends Component {
+  state = {
+    jokes: [],
+  };
+
+  render() {
+    if (!this.state.jokes || !this.state.jokes.length) {
+      return (
+          <div><h1>It's Serious Business Around Here</h1>
+              <h3>Only registered users can access the jokes database.</h3>
+              <p>Please sign up for an account, or sign in if you are already registered.</p></div>
+      )
+  }
+
+    return (
+      <div>
+        <h2>Dad Jokes Galore</h2>
+        <ul>
+        
+       {this.state.jokes.map(j => (
+            <div key={j.id}>
+            <p>{j.setup}</p>
+            <p>{j.punchline}</p>
+            </div>
+            
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');    
+
+    const endpoint = '';
+    const options = {
+      headers: {
+        Authorization: token,
+      },
+    };
+    
+    axios
+      .get(endpoint, options)
+      .then(res => {
+        
+        this.setState({ jokes: res.data }, () => console.log("this.state", this.state));
+        
+        console.log();
+      })
+      .catch(err => {
+        console.error('ERROR', err);
+      });
+  }
+}
+
+export default Users;
